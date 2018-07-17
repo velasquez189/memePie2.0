ab39082009c5a2550465068912fa7ef56ff5474f
Matt McCloskey <matthew.lawrence.mccloskey@gmail.com>
2018-07-17 02:28:28 -0400

    updated aws backend

---
+++ ~awsmobilejs-2018-07-12-13-26-31/#current-backend-info/AppSync.js
@@ -0,0 +1,5 @@
+export default {
+	"graphqlEndpoint": "https://6o3lfbcsjrgylisaoqdnpusfza.appsync-api.us-east-2.amazonaws.com/graphql",
+	"region": "us-east-2",
+	"authenticationType": "AWS_IAM",
+}
