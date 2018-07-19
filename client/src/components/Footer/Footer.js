a6b7c1df558449ccd1be92276d65b3af8cfe6a25
Michael Garland <michaelcgarland@gmail.com>
2018-07-19 18:55:18 -0400

    search button

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from 'react';
+import "./Footer.css"; 
+
 
 const Footer = () => (
     <div className="container navbar-collapse d-block d-lg-none">
         <nav className="navbar fixed-bottom navbar-light bg-light justify-content-between ">
             <ul className="nav nav-pills ">
                 <li className="nav-item">
-                    <a className="nav-link" href="/">Dank</a>
+                    <a className="nav-link footer-link" href="/">Dank</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="upload">Upload</a>
+                    <a className="nav-link footer-link" href="upload">Upload</a>
                 </li>
+                <li className="nav-item">
+                    <a className="nav-link footer-link" href="search">Search</a>
+                </li>
+
             </ul>
         </nav>
     </div>
