7606652ae4a9df9729f69882fd6444d867664210
Michael Garland <mgarland@pmgnet.local>
2018-07-23 14:26:17 -0400

    center upload button

--- client/src/pages/Upload/Upload.js
+++ client/src/pages/Upload/Upload.js
@@ -111,21 +111,23 @@ class Upload extends Component {
     return (
       <Container fluid>
         {/* <Authenticator federated={federated} includeGreetings={true}> */}
-          {/* <S3Album picker /> */}
-          <input id="photoupload" type="file" accept="image/*" onChange={this.handleUpload.bind(this)} />
-          {/* { this.state && <S3Image path={this.state.filePath} /> } */}
-          <form id="tag-form" >
-            <div className="form-group row">
-              {/* <label htmlFor="colFormLabelSm" className="galada-fnt col-sm-2 col-form-label col-form-label-sm">Add a Category:</label> */}
-              <div className="">
+        {/* <S3Album picker /> */}
+        <input id="photoupload" type="file" accept="image/*" onChange={this.handleUpload.bind(this)} />
+        {/* { this.state && <S3Image path={this.state.filePath} /> } */}
+        <form id="tag-form" >
+          <div className="form-group row">
+            {/* <label htmlFor="colFormLabelSm" className="galada-fnt col-sm-2 col-form-label col-form-label-sm">Add a Category:</label> */}
+            <div className="">
               <div className="rules">Tags:</div>
               <div className="rules">MAKE THEM ONE WORD IF YOU WANT PEOPLE TO FIND THEM...</div>
-                <input type="string" className="form-control form-control-sm" id="colFormLabelSm" placeholder="Seperate them with commas!" onChange={this.handleInputChange} />
-                Is this meme Offensive? <input type="checkbox" className="offensive" onChange={this.handleCheckbox} /> 
-              </div>
+              <input type="string" className="form-control form-control-sm" id="colFormLabelSm" placeholder="Seperate them with commas!" onChange={this.handleInputChange} />
+              Is this meme Offensive? <input type="checkbox" className="offensive" onChange={this.handleCheckbox} />
             </div>
+          </div>
+          <div>
             <button id="addphoto" onClick={this.mongoUpload}> Add Photo </button>
-          </form>
+          </div>
+        </form>
         {/* </ Authenticator> */}
       </Container>
     )
@@ -133,4 +135,4 @@ class Upload extends Component {
 };
 
 // export default Upload
-export default withAuthenticator(Upload, {includeGreetings: true, federated: federated});
+export default withAuthenticator(Upload, { includeGreetings: true, federated: federated });
